deaf09fd542fb015be858bf1f8c72c1a451ea63e
Anna Jimenez <annajimenez146@gmail.com>
2019-01-04 21:52:07 -0700

    jthw ex 1-4

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,8 +1,6 @@
 public class FirstProg{
-
   public static void main (String[] args){
-    System.out.println("I am determined that you will learn code");
-    System.out.println("Today is a great day");
+    System.out.println("I, Anna Jimnenez, determined to learn how to code");
+    System.out.println("Today's date is Firday, January 4, 2019");
   }
-
 }
